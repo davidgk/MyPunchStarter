7214c68e4fea7950d8375bff2dafc77dfce9f993
davidgk <dkotlirevsky@newclassrooms.org>
2022-02-17 18:33:05 -0300

    Add contributor functionality

--- smartContract/contracts/MyPunchStarter.sol
+++ smartContract/contracts/MyPunchStarter.sol
@@ -4,8 +4,22 @@ pragma solidity ^0.8.9;
 
 contract MyPunchStarter {
     address public manager;
+    uint public minimumContribution;
+    uint public lastContribution;
+    address[] public  approvers;
 
-    constructor() {
+    constructor ( uint8 minimum) {
         manager = msg.sender;
+        minimumContribution = minimum;
+    }
+
+    function getApprovers() public view returns (address[] memory){
+        return approvers;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+        lastContribution = msg.value;
+        approvers.push(msg.sender);
     }
 }
--- smartContract/test/MyPunchStarter.test.js
+++ smartContract/test/MyPunchStarter.test.js
@@ -11,7 +11,7 @@ const {compileMyPunchStarter} = require("../config/contractCompiler");
 // get the interface and definition from contract , once compiled
 
 
-
+const STD_ERROR_TX = 'VM Exception while processing transaction: revert';
 const localNetworkProvider = ganache.provider();
 const web3 = new Web3(localNetworkProvider)
 
@@ -35,6 +35,7 @@ async function deployContract(contractDeployer, contractCompiled, arguments, acc
 }
 
 describe ('MyPunchStarter Contract tests', () => {
+    const MINIMUN_CONTRIBUTION = web3.utils.toWei("1", 'ether');
     let accounts, campaign, contractDeployer, contractCompiled,account;
     beforeEach(async () => {
         // Get a list of all accounts
@@ -47,21 +48,67 @@ describe ('MyPunchStarter Contract tests', () => {
         // we should pass the interface, under JSON format, said nothing about specific contract
         contractDeployer = new web3.eth.Contract(contractCompiled.abi);
         account = accounts[0];
-        campaign = await deployContract(contractDeployer, contractCompiled, [], account);
     })
-    it( 'obtain accounts from web3', () => {
-       expect(accounts.length > 0).to.be.true;
-    })
-    describe('when we deploy it  with an account ' , () => {
-        it( 'it was successful', async () => {
-            expect(campaign).not.to.be.null;
-            // means that it was successfully deploy if address exists
-            console.log(campaign.options.address)
-            expect(campaign.options.address).not.to.be.null;
-            expect(campaign.options.address.length).to.eq(42);
+    describe('when the campaign has a correct minimumContribution', () => {
+        beforeEach(async () =>{
+            campaign = await deployContract(contractDeployer, contractCompiled, [100], account);
         })
-        it( 'manager was corrected assigned', async () => {
-            expect(await campaign.methods.manager().call()).to.eq(account);
+        it( 'obtain accounts from web3', () => {
+            expect(accounts.length > 0).to.be.true;
+        })
+        it( 'obtain accounts from web3', () => {
+            expect(accounts.length > 0).to.be.true;
+        })
+        describe('when we deploy it  with an account ' , () => {
+            it( 'it was successful', async () => {
+                expect(campaign).not.to.be.null;
+                // means that it was successfully deploy if address exists
+                console.log(campaign.options.address)
+                expect(campaign.options.address).not.to.be.null;
+                expect(campaign.options.address.length).to.eq(42);
+            })
+            it( 'manager was corrected assigned', async () => {
+                expect(await campaign.methods.manager().call()).to.eq(account);
+            })
+            it( 'minimumContribution is assigned', async () => {
+                expect(await campaign.methods.minimumContribution().call()).to.eq(String(100));
+            })
         })
     })
+
+    describe('#contribute', () => {
+        let contributor
+        const VALID_ETHER_VALUE_WEI = web3.utils.toWei("0.01", 'ether');
+        describe('when the campaign has a correct minimumContribution', () => {
+            beforeEach(async () => {0
+                campaign = await deployContract(contractDeployer, contractCompiled, [100], account);
+                contributor = accounts[1]
+            });
+            it('and contributor add the correct amount should be added into approvers', async () => {
+                await campaign.methods.contribute().send({ from: contributor, value: VALID_ETHER_VALUE_WEI  });
+                const contributorSaved = await campaign.methods.approvers(0).call();
+                expect(contributorSaved).to.eq(contributor);
+            })
+            it('and contributor add incorrect amount should throw an error', async () => {
+                try {
+                    await campaign.methods.contribute().send({ from: contributor, value: 20  });
+                    expect.fail('Should fail')
+                } catch (e) {
+                    expect(e.message).to.eq(STD_ERROR_TX)
+                }
+            })
+        })
+    })
+
+    describe('when the campaign has an incorrect minimumContribution', () => {
+        it( 'should throw an error', async () => {
+            try {
+                campaign = await deployContract(contractDeployer, contractCompiled, [-1], account);
+                expect.fail('Should fail')
+            } catch (e) {
+                expect(e.message).to.eq('value out-of-bounds (argument="minimum", value=-1, code=INVALID_ARGUMENT, version=abi/5.0.7)')
+            }
+        })
+    })
+
 })

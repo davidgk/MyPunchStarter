f665a5b749c60590cc56aa42ddd035bc170319ef
davidgk <dkotlirevsky@newclassrooms.org>
2022-02-18 15:11:55 -0300

    Update to zicky 1.0.3

--- smartContract/test/MyPunchStarter.test.js
+++ smartContract/test/MyPunchStarter.test.js
@@ -8,50 +8,25 @@ const Web3 = require('web3')
 const path = require("path");
 const fs = require("fs");
 const {compileMyPunchStarter} = require("../config/contractCompiler");
+const {getDeployManager} = require("zicky");
 // get the interface and definition from contract , once compiled
 
 
-const STD_ERROR_TX = 'VM Exception while processing transaction: revert';
-const localNetworkProvider = ganache.provider();
-const web3 = new Web3(localNetworkProvider)
-
-    /**
-     *  web3 Main task up to now:
-     *  1.- give the accounts unlock to tests our contracts
-     *  2.- Deploy our contracts
-     *  To deploy it only need the ABI and the bitecode
-     *  3.- Communicate through JS apps to our contracts deployed on Network     *
-     *  To interact with the contract we need:
-     *  ABI ( the interface ! )  and the address of deployed contract!
-    */
-
-
-
-async function deployContract(contractDeployer, contractCompiled, arguments, account, value = 0) {
-    return contractDeployer
-        // create the transaction Object to be sent
-        .deploy({data: contractCompiled.evm.bytecode.object, arguments})
-        .send({from: account, gas: 1000000, value});
-}
+const STD_ERROR_TX = 'Transaction has been reverted by the EVM:';
 
 describe ('MyPunchStarter Contract tests', () => {
-    const MINIMUN_CONTRIBUTION = web3.utils.toWei("1", 'ether');
-    let accounts, campaign, contractDeployer, contractCompiled,account;
+    let accounts, campaign, contractDeployer, contractCompiled, account, MINIMUN_CONTRIBUTION;
     beforeEach(async () => {
         // Get a list of all accounts
-        accounts = await web3.eth.getAccounts();
-        // Use one of those accounts to deploy the contract
         contractCompiled = compileMyPunchStarter()
-        // Teaches to web3 about what methods and campaign has, remember
-        // web3 has the bridge role between ou contract and EN
-        // abi is the JS layer between deployed code and us
-        // we should pass the interface, under JSON format, said nothing about specific contract
-        contractDeployer = new web3.eth.Contract(contractCompiled.abi);
+        contractDeployer = await getDeployManager(contractCompiled)
+        MINIMUN_CONTRIBUTION = contractDeployer.getWeb3Object().utils.toWei("1", 'ether');
+        accounts = contractDeployer.accounts;
         account = accounts[0];
     })
     describe('when the campaign has a correct minimumContribution', () => {
         beforeEach(async () =>{
-            campaign = await deployContract(contractDeployer, contractCompiled, [100], account);
+            campaign = await contractDeployer.deployContract(account, [100]);
         })
         it( 'obtain accounts from web3', () => {
             expect(accounts.length > 0).to.be.true;
@@ -68,7 +43,8 @@ describe ('MyPunchStarter Contract tests', () => {
                 expect(campaign.options.address.length).to.eq(42);
             })
             it( 'manager was corrected assigned', async () => {
-                expect(await campaign.methods.manager().call()).to.eq(account);
+                let manager = await campaign.methods.manager().call();
+                expect(manager.toLowerCase()).to.eq(account);
             })
             it( 'minimumContribution is assigned', async () => {
                 expect(await campaign.methods.minimumContribution().call()).to.eq(String(100));
@@ -77,24 +53,25 @@ describe ('MyPunchStarter Contract tests', () => {
     })
 
     describe('#contribute', () => {
-        let contributor
-        const VALID_ETHER_VALUE_WEI = web3.utils.toWei("0.01", 'ether');
+        let contributor, VALID_ETHER_VALUE_WEI
+
         describe('when the campaign has a correct minimumContribution', () => {
-            beforeEach(async () => {0
-                campaign = await deployContract(contractDeployer, contractCompiled, [100], account);
+            beforeEach(async () => {
+                VALID_ETHER_VALUE_WEI = contractDeployer.getWeb3Object().utils.toWei("0.01", 'ether');
+                campaign = await contractDeployer.deployContract(account, [100]);
                 contributor = accounts[1]
             });
             it('and contributor add the correct amount should be added into approvers', async () => {
                 await campaign.methods.contribute().send({ from: contributor, value: VALID_ETHER_VALUE_WEI  });
                 const contributorSaved = await campaign.methods.approvers(0).call();
-                expect(contributorSaved).to.eq(contributor);
+                expect(contributorSaved.toLowerCase()).to.eq(contributor);
             })
             it('and contributor add incorrect amount should throw an error', async () => {
                 try {
                     await campaign.methods.contribute().send({ from: contributor, value: 20  });
                     expect.fail('Should fail')
                 } catch (e) {
-                    expect(e.message).to.eq(STD_ERROR_TX)
+                    expect(e.message.split('\n')[0]).to.eq(STD_ERROR_TX)
                 }
             })
         })
@@ -103,10 +80,10 @@ describe ('MyPunchStarter Contract tests', () => {
     describe('when the campaign has an incorrect minimumContribution', () => {
         it( 'should throw an error', async () => {
             try {
-                campaign = await deployContract(contractDeployer, contractCompiled, [-1], account);
+                campaign = await contractDeployer.deployContract(account, [-1]);
                 expect.fail('Should fail')
             } catch (e) {
-                expect(e.message).to.eq('value out-of-bounds (argument="minimum", value=-1, code=INVALID_ARGUMENT, version=abi/5.0.7)')
+                expect(e.message.split('\n')[0]).to.eq('value out-of-bounds (argument="minimum", value=-1, code=INVALID_ARGUMENT, version=abi/5.0.7)')
             }
         })
     })
